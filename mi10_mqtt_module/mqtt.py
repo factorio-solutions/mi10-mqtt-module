3c30ace6946dfd51b0a954e5bec27e40cd0ac68d
Petr Cezner <petr.cezner@factorio.cz>
2022-03-22 14:11:07 +0100

    add data type

--- mi10_mqtt_module/mqtt.py
+++ mi10_mqtt_module/mqtt.py
@@ -15,7 +15,7 @@ logger = logging.getLogger('mi10-mqtt-module' + '.mqtt.py')
 class MqttClient:
     def __init__(self, host: str, port: int, username: str, password: str, pill2kill: Event, topics: list,
                  module_name: str = 'mod-module',
-                 bind_address: str = '0.0.0.0', presence_frequency: int = 10):
+                 bind_address: str = '0.0.0.0', presence_frequency: int = 10) -> None:
         super().__init__()
         self.host = host
         self.pill2kill = pill2kill
@@ -30,15 +30,15 @@ class MqttClient:
             self._client.username_pw_set(username=username,
                                          password=password)
 
-    def connect(self):
+    def connect(self) -> None:
         self.__setup_connection()
         self.start()
 
-    def start(self):
+    def start(self) -> None:
         self.rt = RepeatedTimer(self.presence_frequency, self.publish, 'mod/presence', {'type': self.module_type})
         self._client.loop_start()
 
-    def stop(self):
+    def stop(self) -> None:
         self._client.loop_stop()
         self.rt.stop()
         self._client.disconnect()
@@ -47,35 +47,35 @@ class MqttClient:
         while not self.pill2kill.wait(0):
             self._client.loop()
 
-    def __setup_connection(self):
+    def __setup_connection(self) -> None:
         self._client.on_connect = self._on_connect
         self._client.connect(host=self.host, port=self.port, bind_address=self.bind_address)
 
-    def _on_end(self, client, userdata, message: MQTTMessage):
+    def _on_end(self, client, userdata, message: MQTTMessage) -> None:
         logger.debug(f"Received message: {str(message.payload)} on topic: {message.topic} with QoS: {str(message.qos)}")
         self._client.disconnect()
         self.pill2kill.set()
 
-    def _on_init(self, client, userdata, message: MQTTMessage):
+    def _on_init(self, client, userdata, message: MQTTMessage) -> None:
         logger.debug(f"Received message: {str(message.payload)} on topic: {message.topic} with QoS: {str(message.qos)}")
         self.publish(topic='mod/discovery/init-response', response={'loaded': True})
 
-    def publish(self, topic, response, qos=0):
+    def publish(self, topic: str, response: dict, qos: int = 0) -> None:
         logger.debug(f'Publishing to topic: {topic} message: {response}')
         self._client.publish(topic, json_util.dumps(response), qos=qos)
 
-    def publish_queue(self, queue):
+    def publish_queue(self, queue) -> None:
         self._client.publish('mod/discovery/data-output', json.dumps(queue))
 
-    def _on_connect(self, client, userdata, flags, rc):
+    def _on_connect(self, client, userdata, flags, rc) -> None:
         logger.info(f'Client connected to host {self.host} on port: {self.port}')
         for topic in self._topics:
             if len(topic) > 1:
                 self._client.message_callback_add(topic[0], topic[1])
             self._client.subscribe(topic[0])
-        # self.__is_present()
+        self.__is_present()
 
-    def __is_present(self):
+    def __is_present(self) -> None:
         self._client.subscribe('mod/presence')
         self._client.publish('mod/presence', json_util.dumps({"type": self.module_type}))
 
